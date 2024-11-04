3a043fc18eb47bf507b835a4ee9cacbfa43d7f28
Ben Davies <ben@owsy.com>
2024-11-04 21:43:02 +0000

    fix: widen return types to mixed

--- src/Qossmic/DataTransformer/ValueObjectTransformer.php
+++ src/Qossmic/DataTransformer/ValueObjectTransformer.php
@@ -43,10 +43,8 @@ final class ValueObjectTransformer implements DataTransformerInterface
 
     /**
      * @param object|null $value
-     *
-     * @return array<string,bool|int|string|null>|bool|int|string|null
      */
-    public function transform(mixed $value): array|bool|int|string|null
+    public function transform(mixed $value): mixed
     {
         if (null === $value) {
             return null;
@@ -90,7 +88,7 @@ final class ValueObjectTransformer implements DataTransformerInterface
         }
     }
 
-    private function getPropertyValue(FormBuilderInterface $form, object $object): bool|int|string|null
+    private function getPropertyValue(FormBuilderInterface $form, object $object): mixed
     {
         if (null !== $form->getPropertyPath()) {
             /* @phpstan-ignore-next-line */
